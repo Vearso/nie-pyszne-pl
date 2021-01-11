e2cf7f6c4ee2f015ec3529ebe17616c6803144f1
Vearso <gus.szymon@gmail.com>
2021-01-11 14:10:45 +0100

    Basic Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: ['./index.html','./src/**/*.{vue,js,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: {
+        light: 'rgba(108, 99, 255, 0.2)',
+        DEFAULT: '#6c63ff',
+      },
+      secondary: {
+        lighter: '#e3e3e3',
+        light: '#b3b3b3' ,
+        DEFAULT: '#acacac',
+        dark: '#3f3d56',
+        darker: 'rgba(63, 61, 86, 0.6)',
+      }
+    },
+    fontFamily: {
+      default: 'Roboto',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
