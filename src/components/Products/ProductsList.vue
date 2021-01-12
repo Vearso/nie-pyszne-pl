e73e320901c0a5d16e0a997f29184f9165067c99
oliwia <ooremek@gmail.com>
2021-01-12 14:23:48 +0100

    Products list view

---
+++ src/components/Products/ProductsList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="product-list">
+    <products-list-item
+      v-for="(item, index) in items"
+      :key="index"
+      :title="item.title"
+      :price="item.price"
+      :imgUrl="item.imgUrl"
+      :rate="item.rate"
+    >
+    </products-list-item>
+  </div>
+</template>
+
+<script lang="ts">
+import ProductsListItem from "@/components/Products/ProductsListItem.vue";
+
+export default {
+  name: "ProductsList",
+  data() {
+    return {
+      items: [
+        {
+          title: "quatrro",
+          price: 15,
+          rate: 5.5,
+          imgUrl:
+            "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        },
+        {
+          title: "quatrro",
+          price: 15,
+          rate: 4.5,
+          imgUrl:
+            "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        },
+        {
+          title: "quatrro",
+          price: 15,
+          rate: 3.5,
+          imgUrl:
+            "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        },
+        {
+          title: "quatrro",
+          price: 15,
+          rate: 4.5,
+          imgUrl:
+            "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        },
+        {
+          title: "quatrro",
+          price: 15,
+          rate: 4.5,
+          imgUrl:
+            "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        },
+        {
+          title: "quatrro",
+          price: 15,
+          rate: 4.5,
+          imgUrl:
+            "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        }
+      ]
+    };
+  },
+  components: {
+    ProductsListItem
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.product-list {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <Header/>
-    <Navigation/>
-    <Products/>
+    <!-- <Header/>
     <Cart/>
+    <Navigation/> -->
+    <products-list/>
+    
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import ProductsList from "../components/Products/ProductsList.vue";
 
 @Options({
   components: {
+    ProductsList
   }
 })
 export default class Home extends Vue {}
