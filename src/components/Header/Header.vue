a75e5b65ec735fa76961d40868962a4e44ee1765
Vearso <gus.szymon@gmail.com>
2021-01-12 11:18:27 +0100

    Header component

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,13 +1,45 @@
 <template>
+  <header class="np-header">
+    <img class="np-header__image"
+         :src="image"
+         alt="Logo">
 
+    <div class="np-header__wrapper">
+      <h1 class="np-header__wrapper__title">{{ title }}</h1>
+      <p class="np-header__wrapper__subtitle">{{ subtitle }}</p>
+    </div>
+  </header>
 </template>
 
-<script>
-export default {
-name: "Header"
+<script lang="ts">
+import {Vue} from "vue-class-component";
+import {Prop} from 'vue-property-decorator';
+
+export default class Header extends Vue {
+  @Prop({default: ''}) readonly image!: string
+  @Prop({default: ''}) readonly title!: string
+  @Prop({default: ''}) readonly subtitle!: string
 }
 </script>
 
-<style scoped>
+<style scoped lang="css">
+.np-header {
+  @apply flex justify-around w-1/2 max-h-28 place-self-center mx-auto mt-20 bg-primary-light rounded-tr-2xl
+}
 
+.np-header__image {
+  @apply w-44 h-44 self-center
+}
+
+.np-header__wrapper {
+  @apply flex flex-col text-left max-w-sm pt-4
+}
+
+.np-header__wrapper__title {
+  @apply text-3xl font-bold text-primary
+}
+
+.np-header__wrapper__subtitle {
+  @apply text-base text-secondary-darker
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <Header/>
+    <Header :image="imageHeader"
+            :title="titleHeader"
+            :subtitle="subtitleHeader"/>
     <Navigation/>
     <Products/>
     <Cart/>
@@ -9,10 +11,18 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-
+import Header from "@/components/Header/Header.vue";
 @Options({
   components: {
+    Header
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private imageHeader =
+      "https://www.flaticon.com/svg/vstatic/svg/2833/2833438.svg?token=exp=1610442341~hmac=78691034b920a1b5599d482745dddbc9"
+  private titleHeader =
+      "Free delivery today!";
+  private subtitleHeader =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing";
+}
 </script>
