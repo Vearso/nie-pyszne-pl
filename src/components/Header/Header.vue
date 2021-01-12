817c7b454f654dcb5886d1a0e9e400efdff958b1
Vearso <gus.szymon@gmail.com>
2021-01-12 14:47:09 +0100

    Changes after pull request

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -12,34 +12,43 @@
 </template>
 
 <script lang="ts">
-import {Vue} from "vue-class-component";
-import {Prop} from 'vue-property-decorator';
+import {defineComponent} from "vue";
 
-export default class Header extends Vue {
-  @Prop({default: ''}) readonly image!: string
-  @Prop({default: ''}) readonly title!: string
-  @Prop({default: ''}) readonly subtitle!: string
-}
+const Header = defineComponent({
+  props: {
+    image: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+    },
+    subtitle: {
+      type: String,
+    }
+  }
+})
+export default Header
 </script>
 
-<style scoped lang="css">
+<style scoped lang="scss">
 .np-header {
-  @apply flex justify-around w-1/2 max-h-28 place-self-center mx-auto mt-20 bg-primary-light rounded-tr-2xl
-}
+  @apply flex justify-around w-1/2 max-h-28 place-self-center mx-auto mt-20 bg-primary-light rounded-tr-2xl;
 
-.np-header__image {
-  @apply w-44 h-44 self-center
-}
+  &__image {
+    @apply w-44 h-44 self-center;
+  }
 
-.np-header__wrapper {
-  @apply flex flex-col text-left max-w-sm pt-4
-}
+  &__wrapper {
+    @apply flex flex-col text-left max-w-sm pt-4;
 
-.np-header__wrapper__title {
-  @apply text-3xl font-bold text-primary
-}
+    &__title {
+      @apply text-3xl font-bold text-primary;
+    }
 
-.np-header__wrapper__subtitle {
-  @apply text-base text-secondary-darker
+    &__subtitle {
+      @apply text-base text-secondary-darker;
+    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <Header :image="imageHeader"
             :title="titleHeader"
             :subtitle="subtitleHeader"/>
+
     <Navigation/>
     <Products/>
     <Cart/>
@@ -10,8 +11,9 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+import {Options, Vue} from "vue-class-component";
 import Header from "@/components/Header/Header.vue";
+
 @Options({
   components: {
     Header
@@ -19,7 +21,7 @@ import Header from "@/components/Header/Header.vue";
 })
 export default class Home extends Vue {
   private imageHeader =
-      "https://www.flaticon.com/svg/vstatic/svg/2833/2833438.svg?token=exp=1610442341~hmac=78691034b920a1b5599d482745dddbc9"
+      "https://www.flaticon.com/svg/vstatic/svg/2833/2833438.svg?token=exp=1610442341~hmac=78691034b920a1b5599d482745dddbc9";
   private titleHeader =
       "Free delivery today!";
   private subtitleHeader =
