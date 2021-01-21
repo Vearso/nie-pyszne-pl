a1ab52a5187709e16b983d9593b601d76cfce3d9
mnieznaj <marcin.nieznaj@gmail.com>
2021-01-21 15:04:31 +0100

    firebase get request test

--- src/utilities/apiCalls.ts
+++ src/utilities/apiCalls.ts
@@ -1,7 +1,28 @@
-const api = "http://localhost:3000";
+const api =
+  "https://nie-pyszne-pl-13352-default-rtdb.europe-west1.firebasedatabase.app";
+
+type RequestTypes = "GET" | "POST" | "PUT" | "DELETE";
+
+interface RequestSettings {
+  method?: RequestTypes;
+  headers: {
+    "Content-Type": "application/json";
+  };
+  mode: "cors";
+}
+
+const settings = (method: RequestTypes = "GET"): RequestSettings => {
+  return {
+    method,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    mode: "cors"
+  };
+};
 
 export function getFoodList() {
-  return fetch(`${api}/productList`)
+  return fetch(`${api}/productList`, settings())
     .then(res => res.json())
     .catch(err => console.log(err));
 }
